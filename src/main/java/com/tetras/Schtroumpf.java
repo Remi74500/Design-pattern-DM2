b852f2e566852358f5370602c3542cf56fe3c7f1
BARRY Guillaume <guillaume.barry@soprabanking.com>
2019-12-26 16:36:56 +0100

    Add initial classes

---
+++ src/main/java/com/tetras/Schtroumpf.java
@@ -0,0 +1,7 @@
+package com.tetras;
+
+public class Schtroumpf {
+    public void saluer(Animal animal) {
+        System.out.print("Bonjour " + animal.getName());
+    }
+}
---
+++ src/test/java/com/tetras/VillageTest.java
@@ -0,0 +1,40 @@
+package com.tetras;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class VillageTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void testEntre() {
+        Schtroumpf schtroumpfDeGarde = new Schtroumpf();
+        Animal pingouin = new Animal("pingouin");
+        schtroumpfDeGarde.saluer(pingouin);
+        assertEquals("Bonjour pingouin", outContent.toString());
+    }
+}
